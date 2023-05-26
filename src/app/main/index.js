d9d47f03b5235063fd005f756ff226b6015149a0
Kris <Krismail>
2023-05-26 23:02:18 +0300

    feat: add paginations

--- src/app/main/index.js
+++ src/app/main/index.js
@@ -1,4 +1,4 @@
-import {memo, useCallback, useEffect} from 'react';
+import { memo, useCallback, useEffect } from 'react';
 import Item from "../../components/item";
 import PageLayout from "../../components/page-layout";
 import Head from "../../components/head";
@@ -6,6 +6,7 @@ import BasketTool from "../../components/basket-tool";
 import List from "../../components/list";
 import useStore from "../../store/use-store";
 import useSelector from "../../store/use-selector";
+import Paginations from '../../components/paginations';
 
 function Main() {
 
@@ -18,7 +19,10 @@ function Main() {
   const select = useSelector(state => ({
     list: state.catalog.list,
     amount: state.basket.amount,
-    sum: state.basket.sum
+    sum: state.basket.sum,
+    limit: state.catalog.limit,
+    totalArticlesCount: state.catalog.totalArticlesCount,
+    currentPage: state.catalog.currentPage
   }));
 
   const callbacks = {
@@ -26,20 +30,26 @@ function Main() {
     addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
     // Открытие модалки корзины
     openModalBasket: useCallback(() => store.actions.modals.open('basket'), [store]),
+    // Выбор страницы товаров
+    onClickPagination: useCallback((page) => store.actions.catalog.setCurrentPage(page), [store]),
   }
 
   const renders = {
     item: useCallback((item) => {
-      return <Item item={item} onAdd={callbacks.addToBasket}/>
+      return <Item item={item} onAdd={callbacks.addToBasket} />
     }, [callbacks.addToBasket]),
   };
 
   return (
     <PageLayout>
-      <Head title='Магазин'/>
+      <Head title='Магазин' />
       <BasketTool onOpen={callbacks.openModalBasket} amount={select.amount}
-                  sum={select.sum}/>
-      <List list={select.list} renderItem={renders.item}/>
+        sum={select.sum} />
+      <List list={select.list} renderItem={renders.item} />
+      <Paginations totalArticlesCount={select.totalArticlesCount}
+        limit={select.limit}
+        currentPage={select.currentPage}
+        onClickPagination={callbacks.onClickPagination} />
     </PageLayout>
 
   );
